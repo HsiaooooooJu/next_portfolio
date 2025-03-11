dda3ba488e0acaac6e9e5758bb01a5f2978e87fc
hsiaoju <hsiaojuko@liontravel.com>
2025-03-11 23:27:15 +0800

    feat: add ui components and common CSS

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,17 +1,17 @@
 import { Poppins, Gochi_Hand, Source_Code_Pro } from 'next/font/google'
 
 export const poppins = Poppins({
-	subsets: ['latin'],
-	weight: ['200', '300', '400', '500', '700'],
-	style: ['normal', 'italic'],
+    subsets: ['latin'],
+    weight: ['200', '300', '400', '500', '700'],
+    style: ['normal', 'italic'],
 })
 
 export const gochiHand = Gochi_Hand({
-	subsets: ['latin'],
-	weight: ['400']
+    subsets: ['latin'],
+    weight: ['400'],
 })
 
 export const code = Source_Code_Pro({
-	subsets:['latin'],
-	weight: ['500', '700']
+    subsets: ['latin'],
+    weight: ['500', '700'],
 })
